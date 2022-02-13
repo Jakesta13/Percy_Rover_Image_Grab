661d0f493789bb8d663666d54329e5cfb89cff1e
Jake <10874330+Jakesta13@users.noreply.github.com>
2022-02-13 14:59:25 -0500

    Bug Squished! .. I think

--- mars2020.php
+++ mars2020.php
@@ -158,17 +158,17 @@ if (isset($pgcount)){
 						// Folder index, organized by SOL date.
 						$folder_index = ("sol".$grab[$key]['sol']);
 						$folder_index = (preg_replace("/[^A-Za-z0-9 ]/", '', $folder_index));
-//						if (!file_exists('images/'.$folder_index)){
+						if (!file_exists('images/'.$folder_index)){
 							mkdir('images/'.$folder_index, 0777, true);
 //							print_r($folder_index);
-//						};
+						};
 
 						$folder_name = ($grab[$key]['title']);
 						// https://stackoverflow.com/a/2303377
 						$folder_name = (preg_replace("/[^A-Za-z0-9 ]/", '', $folder_name));
 						// https://stackoverflow.com/questions/12704613/php-str-replace-replace-spaces-with-underscores
 						$folder_name = (preg_replace('/\s+/', '_', $folder_name));
-						if (!file_exists('images/'.$folder_name)) {
+						if (!file_exists('images/'.$folder_index.'/'.$folder_name)) {
 							 mkdir('images/'.$folder_index.'/'.$folder_name, 0777, true);
 						};
 					}else {
